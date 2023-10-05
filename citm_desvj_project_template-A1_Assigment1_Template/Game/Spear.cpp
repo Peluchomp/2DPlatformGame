0a5848c8c9aa33baaf6d0db536032340049b643a
Peluchomp <pau.mora.sole@gmail.com>
2023-10-05 19:24:15 +0200

    Spear cpp and h created

---
+++ citm_desvj_project_template-A1_Assigment1_Template/Game/Spear.cpp
@@ -0,0 +1,52 @@
+#include "Spear.h"
+#include "Source/App.h"
+#include "Source/Textures.h"
+#include "Source/Audio.h"
+#include "Source/Input.h"
+#include "Source/Render.h"
+#include "Source/Scene.h"
+#include "Source/Log.h"
+#include "Source/Point.h"
+#include "Source/Physics.h"
+
+Spear::Spear() : Entity(EntityType::SPEAR)
+{
+	name.Create("spear");
+}
+
+Spear::~Spear() {}
+
+bool Spear::Awake() {
+
+	position.x = parameters.attribute("x").as_int();
+	position.y = parameters.attribute("y").as_int();
+	texturePath = parameters.attribute("texturepath").as_string();
+
+	return true;
+}
+
+bool Spear::Start() {
+
+	//initilize textures
+	texture = app->tex->Load(texturePath);
+	pbody = app->physics->CreateCircle(position.x + 16, position.y + 16, 16, bodyType::DYNAMIC);
+	pbody->ctype = ColliderType::SPEAR;
+
+	return true;
+}
+
+bool Spear::Update(float dt)
+{
+	// L07 DONE 4: Add a physics to an item - update the position of the object from the physics.  
+	position.x = METERS_TO_PIXELS(pbody->body->GetTransform().p.x) - 16;
+	position.y = METERS_TO_PIXELS(pbody->body->GetTransform().p.y) - 16;
+
+	app->render->DrawTexture(texture, position.x, position.y);
+
+	return true;
+}
+
+bool Spear::CleanUp()
+{
+	return true;
+}
---
+++ citm_desvj_project_template-A1_Assigment1_Template/Game/Spear.h
@@ -0,0 +1,36 @@
+#ifndef __SPEAR_H__
+#define __SPEAR_H__
+
+#include "Source/Entity.h"
+#include "Source/Point.h"
+#include "SDL/include/SDL.h"
+
+struct SDL_Texture;
+
+class Spear : public Entity
+{
+public:
+
+	Spear();
+	virtual ~Spear();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update(float dt);
+
+	bool CleanUp();
+
+public:
+
+	bool isPicked = false;
+
+private:
+
+	SDL_Texture* texture;
+	const char* texturePath;
+	PhysBody* pbody;
+};
+
+#endif // __SPEAR_H__

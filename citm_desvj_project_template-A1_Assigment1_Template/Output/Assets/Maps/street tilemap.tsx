29d93fd193468b6083055c24b02b98bb3eab9703
VM Marco <marcoxativa@gmail.com>
2023-10-08 11:37:21 +0200

    The tilest kinda appears
    I manually modified the tmx to avoid the automatic encoding of data but this seems to "corrupt the file somehow". This isn't a problem in the example though... we'll have to keep testing

---
+++ citm_desvj_project_template-A1_Assigment1_Template/Output/Assets/Maps/street tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="street tilemap" tilewidth="40" tileheight="40" tilecount="144" columns="16">
+ <image source="../../../../tilemaps/street tilemap.png" width="640" height="360"/>
+</tileset>
